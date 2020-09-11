84fa9173484f970133f0e34da272dfa34bf1a80e
João Victor D <joaovictorddm@outlook.com>
2020-09-11 18:50:25 -0300

    Novos controllers, models e routes. Metódos index e store criados e funcionando

---
+++ MeuImovel/app/Http/Controllers/Api/RealStateController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\RealState;
+use Illuminate\Http\Request;
+
+class RealStateController extends Controller
+{
+    private $realState;
+
+    public function __construct(RealState $realState)
+    {
+        $this->realState = $realState;
+    }
+
+    public function index()
+    {
+        $realState = $this->realState->paginate('10');
+        return response()->json($realState, 200);
+    }
+
+    public function store(Request $request)
+    {
+        return response()->json($request->all(), 200);
+    }
+
+}
--- MeuImovel/routes/api.php
+++ MeuImovel/routes/api.php
@@ -1,19 +1,16 @@
 <?php
 
+use App\Http\Controllers\Api\RealStateController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::prefix('v1')->namespace('Api')->group(function() {
+    Route::prefix('real_states')->group(function() {
+        Route::get('/', [RealStateController::class, 'index']);
+        Route::post('/', [RealStateController::class, 'store']);
+    });
 });
